4941b23270693d776c60a1f6b2e4a389dc7393d5
samrao1 <samrao1973@gmail.com>
2017-11-30 14:47:19 -0500

    this is the working commit

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,65 +1,38 @@
-// import React, { Component } from 'react';
-// import { connect } from 'react-redux';
-// import { employeeUpdate } from '../actions';
-// import { Card, CardSection, Input, Button } from './common';
-//
-// class EmployeeCreate extends Component {
-//   render() {
-//     return (
-//       <Card>
-//         <CardSection>
-//           <Input
-//             label="Name"
-//             placeholder="Jane"
-//             value={this.props.name}
-//             onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
-//           />
-//         </CardSection>
-//
-//         <CardSection>
-//           <Input
-//             label="Phone"
-//             placeholder="555-555-5555"
-//             value={this.props.phone}
-//             onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
-//           />
-//
-//         </CardSection>
-//
-//         <CardSection>
-//           <Button>
-//             Create
-//           </Button>
-//         </CardSection>
-//       </Card>
-//     );
-//   }
-// }
-// const mapStateToProps = (state) => {
-//   const { name, phone, shift } = state.employeeForm;
-//     return { name, phone, shift };
-// };
-
-//
-// export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
 import React, { Component } from 'react';
+//import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { employeeUpdate, employeeCreate } from '../actions';
-import { Card, CardSection, Button } from './common';
-//import EmployeeForm from './EmployeeForm';
+import { employeeUpdate } from '../actions';
+import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
-  onButtonPress() {
-    const { name, phone, shift } = this.props;
-
-    this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
-  }
-
   render() {
     return (
+    //
+    // <View>
+    //  <Text>Employees</Text>
+    // </View>
       <Card>
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>
+          <Input
+            label="Name"
+            placeholder="Jane"
+            value={this.props.name}
+            onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Phone"
+            placeholder="555-555-5555"
+            value={this.props.phone}
+            onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
+          />
+
+        </CardSection>
+
+        <CardSection>
+          <Button>
             Create
           </Button>
         </CardSection>
@@ -67,13 +40,46 @@ class EmployeeCreate extends Component {
     );
   }
 }
-
 const mapStateToProps = (state) => {
   const { name, phone, shift } = state.employeeForm;
 
-  return { name, phone, shift };
+    return { name, phone, shift };
 };
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
 
-export default connect(mapStateToProps, {
-  employeeUpdate, employeeCreate
-})(EmployeeCreate);
+
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import { employeeUpdate, employeeCreate } from '../actions';
+// import { Card, CardSection, Button } from './common';
+// //import EmployeeForm from './EmployeeForm';
+//
+// class EmployeeCreate extends Component {
+//   onButtonPress() {
+//     const { name, phone, shift } = this.props;
+//
+//     this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+//   }
+//
+//   render() {
+//     return (
+//       <Card>
+//         <CardSection>
+//           <Button onPress={this.onButtonPress.bind(this)}>
+//             Create
+//           </Button>
+//         </CardSection>
+//       </Card>
+//     );
+//   }
+// }
+//
+// const mapStateToProps = (state) => {
+//   const { name, phone, shift } = state.employeeForm;
+//
+//   return { name, phone, shift };
+// };
+//
+// export default connect(mapStateToProps, {
+//   employeeUpdate, employeeCreate
+// })(EmployeeCreate);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 //this is linking the authreducers from the other file
 //to this file, which brings all reducers together
 import AuthReducer from './AuthReducer';
+import EmployeeFormReducer from './EmployeeFormReducer';
 
 //this call exports all reducers
 export default combineReducers({
 //the key from the pair below is what is carrying the state
 //this is like saying "the "auth" piece of state is produced by the authreducer
   auth: AuthReducer,
+  employeeForm: EmployeeFormReducer
 });
